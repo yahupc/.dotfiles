3662cc3e6ab4f7d78fcc9c5de4b206033b53f472
yahu39 <yahu39pc@gmail.com>
2023-06-29 04:12:03 -0500

    null-ls lsp settings for php

--- .config/nvim/lua/custom/configs/null-ls.lua
+++ .config/nvim/lua/custom/configs/null-ls.lua
@@ -7,14 +7,13 @@ end
 local b = null_ls.builtins
 
 local sources = {
-
   -- webdev stuff
   b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
   b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
-
   -- Lua
   b.formatting.stylua,
-
+  --php
+  b.formatting.phpcsfixer,
   -- cpp
   b.formatting.clang_format,
 }
