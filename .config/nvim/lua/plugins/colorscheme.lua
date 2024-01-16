3853e14d4c638ee63df36938cbc29aec0219a99f
yahupc <yahu39pc@gmail.com>
2024-01-16 11:28:40 -0500

    php configuration

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,12 @@
 return {
-  {
-    "craftzdog/solarized-osaka.nvim",
-    branch = "osaka",
-    lazy = true,
-    priority = 1000,
-    opts = function()
-      return {
-        transparent = false,
-      }
-    end,
-  },
+	{
+		"craftzdog/solarized-osaka.nvim",
+		lazy = true,
+		priority = 1000,
+		opts = function()
+			return {
+				transparent = true,
+			}
+		end,
+	},
 }
